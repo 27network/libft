e7e2f6a59f0a41810667faa3f42bb3ce244e6383
Killian <oss@xtrm.me>
2023-11-21 21:38:20 +0100

    🐛 fix(norm): fix norm error

--- src/print/printf/str/pf_str_init.c
+++ src/print/printf/str/pf_str_init.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 17:21:37 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/11/17 03:32:55 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/11/21 21:38:16 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	pf_str_init(t_str *self, char *str, size_t length)
 {
 	if (!str)
 		length = 0;
-	self->inner = str; 
+	self->inner = str;
 	self->length = length;
 }
