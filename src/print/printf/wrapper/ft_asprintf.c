3c2eebda331d8ba63db0325b5b5b5b67d2912533
xtrm <oss@xtrm.me>
2023-11-21 05:14:28 +0100

    🐛 fix(ft_asprintf.c): wrong function name

--- src/print/printf/wrapper/ft_asprintf.c
+++ src/print/printf/wrapper/ft_asprintf.c
@@ -6,13 +6,13 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 02:26:47 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/11/17 03:32:54 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/11/21 05:13:19 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/internal/printf.h>
 
-int	ft_sprintf(char **str, const char *format, ...)
+int	ft_asprintf(char **str, const char *format, ...)
 {
 	va_list	args;
 	int		len;
