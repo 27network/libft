4a9670603ab5fe9f763f4efbc40a1e365d5bf8c8
Killian <oss@xtrm.me>
2023-12-20 15:09:42 +0100

    ✨ feature(print/printf): remove usage of global var for ugly static getter

--- src/print/printf/pf_format_spec.c
+++ src/print/printf/pf_format_spec.c
@@ -6,25 +6,30 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 11:24:30 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/11/17 21:25:20 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:09:06 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/internal/printf.h>
 
-static const t_type_formatter	g_formatters[] = {
-['c'] = pf_format_char,
-['s'] = pf_format_string,
-['p'] = pf_format_pointer,
-['d'] = pf_format_int,
-['i'] = pf_format_int,
-['u'] = pf_format_uint,
-['x'] = pf_format_hex,
-['X'] = pf_format_hex,
-['%'] = pf_format_percent,
-['n'] = pf_format_lenptr,
-['m'] = pf_format_strerror,
-};
+static t_type_formatter	pf_get_formatter(char specifier)
+{
+	static t_type_formatter	formatters[] = {
+	['c'] = pf_format_char,
+	['s'] = pf_format_string,
+	['p'] = pf_format_pointer,
+	['d'] = pf_format_int,
+	['i'] = pf_format_int,
+	['u'] = pf_format_uint,
+	['x'] = pf_format_hex,
+	['X'] = pf_format_hex,
+	['%'] = pf_format_percent,
+	['n'] = pf_format_lenptr,
+	['m'] = pf_format_strerror
+	};
+
+	return (formatters[(int)specifier]);
+}
 
 static char	*pf_apply_flag_mutators(t_fmt_spec *spec, char *str)
 {
@@ -49,7 +54,7 @@ static char	*pf_apply_formatters(t_fmt_spec *spec, va_list args, int *hnc)
 	*hnc = 0;
 	if (ft_strchr(PF_SPECIFIERS, spec->specifier))
 	{
-		formatter = g_formatters[(int)spec->specifier];
+		formatter = pf_get_formatter(spec->specifier);
 		if (formatter)
 			result = formatter(spec, args);
 		if (result && spec->specifier == 'c' && *result == '\0')
