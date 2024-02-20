83680d25a1daee51c4a25738c7a74a6bc50779a8
xtrm <oss@xtrm.me>
2024-02-20 19:34:58 +0100

    ✅ test(hash_map): added freeing cuz yea :D
    Signed-off-by: xtrm <oss@xtrm.me>

--- tests/hash_map/src/lifetime.c
+++ tests/hash_map/src/lifetime.c
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   creation.c                                         :+:      :+:    :+:   */
+/*   lifetime.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:10:29 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/02/20 19:18:54 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/02/20 19:34:34 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 #include <ft/data/map.h>
 
-DEFINE_TEST(create, "Can create an empty map", {
+DEFINE_TEST(create, "Can create and free a map", {
 	t_map	*map;
 
 	map = ft_map_new(10, NULL, NULL);
-	ASSERT(map != NULL);
+	ASSERT_NEQ(map, NULL);
 	ft_map_free(&map, NULL);
+	ASSERT_EQ(map, NULL);
 })
