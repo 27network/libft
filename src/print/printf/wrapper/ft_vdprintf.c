d721b505a7013c67a4f2e4664e96050c5a39caf9
xtrm <oss@xtrm.me>
2024-03-03 06:27:34 +0100

    🐛 fix(write): fix warnings

--- src/print/printf/wrapper/ft_vdprintf.c
+++ src/print/printf/wrapper/ft_vdprintf.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 02:27:01 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/11/17 03:29:27 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/03/03 06:21:32 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_vdprintf(int fd, const char *format, va_list args)
 	str = NULL;
 	len = ft_vasprintf(&str, format, args);
 	if (len > 0 && str)
-		write(fd, str, len);
+		(void) write(fd, str, len);
 	if (str)
 		free(str);
 	return (len);
