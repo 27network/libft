efcff91a6c624bd95614bae6a2cae53c79f4e666
xtrm <oss@xtrm.me>
2024-05-18 02:42:59 +0200

    ✨ feature(flake.nix): added doxygen to native inputs

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
           LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib";
 
           nativeBuildInputs = with pkgs; [
+            doxygen
             norminette
             valgrind
             gdb
