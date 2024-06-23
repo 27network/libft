b2df79831b8c3faa4367f89caf842c0d0cbdfec1
xtrm <oss@xtrm.me>
2024-06-23 06:35:35 +0200

    🐛 fix(0.6.3.1): actually think for a second goddamn
    Signed-off-by: xtrm <oss@xtrm.me>

--- src/mem/ft_realloc.c
+++ src/mem/ft_realloc.c
@@ -6,30 +6,30 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 06:26:50 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/06/23 06:28:05 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/06/23 06:35:06 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft/mem.h>
 
-void	*ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
 	void	*new_ptr;
 
 	if (!ptr)
-		return (ft_calloc(size, 1));
-	if (size == 0)
+		return (ft_calloc(new_size, 1));
+	if (!old_size || !new_size)
 	{
 		free(ptr);
 		return (ft_calloc(1, 1));
 	}
-	new_ptr = ft_calloc(size, 1);
+	new_ptr = ft_calloc(new_size, 1);
 	if (!new_ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	ft_memcpy(new_ptr, ptr, size);
+	ft_memcpy(new_ptr, ptr, old_size);
 	free(ptr);
 	return (new_ptr);
 }
